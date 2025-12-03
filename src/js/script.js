c9ce12850b89ade23a95e1842d8c4040b39669f5
emma-bdrs <emma.badarous@edu.univ-fcomte.fr>
2025-12-03 14:17:58 +0100

    commit

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,12 @@
-// *** LIGHTBOX ***
+
+const lightBox = document.querySelector("#lightbox");
+
+const lightBoxImg = document.querySelector("#lightbox img");
+
+lightBox?.addEventListener("click", () => lightBox.close());
+
+document.body.addEventListener("click", (evt) => {
+  if (!evt.target.matches("[data-full-img]")) return;
+  lightBoxImg.src = evt.target.dataset.fullImg;
+  lightBox.showModal();
+});
